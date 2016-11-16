eb957809b14fa8863ffa05047eb5bdc069cb6b71
Louis Henri Franc <louis.franc@insa-rouen.fr>
2016-11-16 12:30:16 -0500

    implementation done, need to add capacity to the bob network

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Simple implementation of [Learning to Protect Communications with Adversarial Neural Cryptography](https://arxiv.org/abs/1610.06918) in 100 lines using only simple Dense Layers. 
+
+# Dependencies
+* Theano
+* Numpy
+* Lasagne
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def get_init(name):
     if name == "glorotU":
-        return lasagne.init.GlorotUniform
+        return lasagne.init.GlorotUniform()
 
 
 def get_nonlinearity(name):
@@ -27,7 +27,7 @@ def gen_data(n, msg_len, key_len):
 class HiddenLayer(object):
     def __init__(self, inputs, size_batch, input_size, n_hidden, n_out, name, nonlinearity="relu", depth=1):
         self.depth = depth
-        self.l_in = InputLayer((size_batch, input()))
+        self.l_in = InputLayer((size_batch, input_size), input_var=inputs)
         self.l_hid = DenseLayer(self.l_in,
                                 num_units=n_hidden,
                                 nonlinearity=get_nonlinearity(nonlinearity),
@@ -46,7 +46,7 @@ class HiddenLayer(object):
         return get_output(self.l_hid)
 
     def get_params(self):
-        params = get_all_params(self.l_hid)
+        return get_all_params(self.l_hid)
 
 
 class AdversarialNeuralCryptoNet(object):
@@ -54,7 +54,7 @@ class AdversarialNeuralCryptoNet(object):
         self.SIZE_BATCH = 512
         self.SIZE_KEY = 16
         self.SIZE_MESSAGE = 16
-        self.N_HIDDEN = 16
+        self.N_HIDDEN = 32
 
         ### BUILD THE MODELS
         X_msg = T.matrix('msg')
@@ -70,7 +70,7 @@ class AdversarialNeuralCryptoNet(object):
                                 "alice")
 
         # Create Bob neural network
-        bob_MLP = HiddenLayer(alice_MLP.get_output(),
+        bob_MLP = HiddenLayer(T.concatenate([alice_MLP.get_output(), X_key], axis=1),
                               self.SIZE_BATCH,
                               self.SIZE_KEY + self.SIZE_MESSAGE,
                               self.N_HIDDEN,
@@ -78,9 +78,9 @@ class AdversarialNeuralCryptoNet(object):
                               "bob")
 
         # Create Eve neural network
-        eve_MLP = HiddenLayer(input,
+        eve_MLP = HiddenLayer(alice_MLP.get_output(),
                               self.SIZE_BATCH,
-                              self.SIZE_KEY + self.SIZE_MESSAGE,
+                              self.SIZE_MESSAGE,
                               self.N_HIDDEN,
                               self.SIZE_MESSAGE,
                               "eve", depth=3)
@@ -89,21 +89,24 @@ class AdversarialNeuralCryptoNet(object):
         eve_loss = T.mean(T.abs_(X_msg - eve_MLP.get_output()))
 
         bob_loss = T.mean(T.abs_(X_msg - bob_MLP.get_output())) \
-                   + (self.SIZE_MESSAGE / 2 - eve_err) ** 2 / (self.SIZE_MESSAGE / 2)
-        loss = {'bob': lasagne.updates.adagrad(bob_err)}
-        loss = {'eve': lasagne.updates.adagrad(eve_err)}
+                   + (1. - eve_loss) ** 2
 
         # PARAMS
-        # update bob-alice network
-        params = {'bob': [bob_MLP.get_params(), alice_MLP.get_params()]}
-        self.train_fn = {'bob': theano.function([X_msg, X_key], bob_loss, updates=loss['bob'])}
-        self.test_fn = {'bob': theano.function([X_msg, X_key], bob_loss)}
+        params, losses = {}, {}
+        params['bob'] = bob_MLP.get_params()
+        params['eve'] = eve_MLP.get_params()
 
-        params = {'eve': [eve_MLP.get_params()]}
-        self.train_fn = {'eve': theano.function([X_msg], eve_loss, updates=params['eve'])}
-        self.testfn = {'eve': theano.function([X_key], eve_loss)}
+        losses = {'bob': lasagne.updates.adadelta(bob_loss, params['bob'])}
 
-    def train(self, bob_or_eve, results, max_iters, print_every, es, es_limit=100):
+        self.train_fn, self.test_fn = {}, {}
+        self.train_fn['bob'] = theano.function([X_msg, X_key], bob_loss, updates=losses['bob'])
+        self.test_fn['bob'] = theano.function([X_msg, X_key], bob_loss)
+
+        losses['eve'] = lasagne.updates.adadelta(eve_loss, params['eve'])
+        self.train_fn['eve'] = theano.function([X_msg, X_key], eve_loss, updates=losses['eve'])
+        self.test_fn['eve'] = theano.function([X_msg, X_key], eve_loss)
+
+    def train(self, bob_or_eve, max_iters, print_every, es, es_limit=100):
         count = 0
         for i in range(max_iters):
             msg_in_val, key_val = gen_data(self.SIZE_BATCH, self.SIZE_MESSAGE, self.SIZE_KEY)
@@ -120,11 +123,11 @@ class AdversarialNeuralCryptoNet(object):
 if __name__ == '__main__':
     adversarial_iterations = 60
     adv = AdversarialNeuralCryptoNet()
-    print('LA')
+
     for i in range(adversarial_iterations):
         n = 2000
         print_every = 100
         print('Training bob and alice')
-        adv.train('bob', None, n, print_every, es=0.01)
+        adv.train('bob', n, print_every, es=0.01)
         print('Training eve')
-        adv.train('eve', None, n, print_every, es=0.01)
+        adv.train('eve', n, print_every, es=0.01)
